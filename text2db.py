2f043c2a5a20043b48a46c907bf42e64494e9672
Roberto Martinez <rabdelkader.martinez@servifactory.com>
2011-11-16 12:27:13 +0100

    Otra

--- text2db.py
+++ text2db.py
@@ -58,8 +58,8 @@ def usage():
 
 if __name__ == '__main__':
     version = "1.0"
-    revision = "$Date$"
-    # $Date$
+    revision = "$Date $"
+    # $Date $
 
     c = ConfigParser.RawConfigParser()
 
