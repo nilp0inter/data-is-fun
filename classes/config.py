c387bacac9768030de8598c3a81dc6aa4abe05ea
Roberto Abdelkader <robertomartinezp@gmail.com>
2011-11-27 19:19:13 -0500

    copyright and license

--- classes/config.py
+++ classes/config.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+#
+#    Copyright (C) 2011 Roberto A. Martinez Perez
+#
+#    This file is part of data-is-fun.
+#
+#    data-is-fun is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    data-is-fun is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with data-is-fun.  If not, see <http://www.gnu.org/licenses/>.
+
 """Clase de configuracion basica sobre ficheros de texto.
 
 """
@@ -8,11 +26,11 @@ import ConfigParser
 
 __author__ = "Roberto Abdelkader"
 __credits__ = ["Roberto Abdelkader"]
-__license__ = "GPL"
-__version__ = "1.0"
+__copyright__ = "Copyright (C) 2011 Roberto A. Martinez Perez"
+__license__ = "GPL v3"
+__version__ = "2.0"
 __maintainer__ = "Roberto Abdelkader"
-__email__ = "contacto@robertomartinez.es"
-__status__ = "Production"
+__email__ = "contacto@robertomartinezp.es"
 
 
 class Config:
--- classes/transformers.py
+++ classes/transformers.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+#
+#    Copyright (C) 2011 Roberto A. Martinez Perez
+#
+#    This file is part of data-is-fun.
+#
+#    data-is-fun is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    data-is-fun is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with data-is-fun.  If not, see <http://www.gnu.org/licenses/>.
+
 """Transformadores para text2db.
 
 Un transformador es un objeto que obtiene como entrada una cadena y
@@ -16,12 +34,11 @@ from MySQLdb import escape_string as escape
 
 __author__ = "Roberto Abdelkader"
 __credits__ = ["Roberto Abdelkader"]
-__license__ = "GPL"
-__version__ = "1.0"
+__copyright__ = "Copyright (C) 2011 Roberto A. Martinez Perez"
+__license__ = "GPL v3"
+__version__ = "2.0"
 __maintainer__ = "Roberto Abdelkader"
-__email__ = "contacto@robertomartinez.es"
-__status__ = "Production"
-
+__email__ = "contacto@robertomartinezp.es"
 
 from config import Config
 from topological_sort import robust_topological_sort as ts
